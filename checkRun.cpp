f86c56f578bba0b21b3b0ae99e9dfa1ca6744990
Ss97 <xsufyan@gmail.com>
2017-05-14 23:56:46 +0700

    Add files via upload

---
+++ checkRun.cpp
@@ -0,0 +1,15 @@
+/*=============Code Example===============
+Author	: Sufyan Saori
+Site	: aroundinforamtics.blogspot.co.id
+Code	: Try Run IDE Dev-C++
+==========================================*/
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout<<"Hello World"<<endl;
+	cout<<"it's must be 2 : "<<(1+1)<<endl;
+	return 0;
+}
+
+
